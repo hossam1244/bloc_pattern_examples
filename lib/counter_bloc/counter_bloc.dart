ce2a4016a0a850da8e93cece4acfb2e0347f8e09
Hossam Rakha <hossamrakha0@gmail.com>
2022-06-05 21:41:57 +0200

    added couple of events by the bloc

--- lib/counter_bloc/counter_bloc.dart
+++ lib/counter_bloc/counter_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 enum CounterEvent { increment, decrement }
@@ -13,10 +14,29 @@ class CounterBloc extends Bloc<CounterEvent, int> {
     switch (event) {
       case CounterEvent.increment:
         emitter(state + 1);
+        // emitter(null); // to force onError to get triggered
         break;
       case CounterEvent.decrement:
         emitter(state - 1);
         break;
     }
+  }
+
+  @override
+  void onEvent(CounterEvent event) {
+    super.onEvent(event);
+    print("onEventTriggered $event");
+  }
+
+  @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    super.onTransition(transition);
+    print("onTransition $transition");
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    print("onError $error StackTrace $stackTrace");
   }
 }
