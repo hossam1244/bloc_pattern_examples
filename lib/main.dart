d820dc7deefcc72268e05ca850584977088cc1d2
Hossam Rakha <hossamrakha0@gmail.com>
2022-06-09 21:10:08 +0200

    Added a shop cart example

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:bloc_pattern/counter_bloc/refactor_counter_page.dart';
 import 'package:bloc_pattern/counter_bloc_cubit/counter_cubit.dart';
 import 'package:bloc_pattern/custom_bloc_event/custom_bloc.dart';
 import 'package:bloc_pattern/home_page/home_page_bloc.dart';
+import 'package:bloc_pattern/shopping_cart_bloc/bloc/catalog_bloc.dart';
+import 'package:bloc_pattern/shopping_cart_bloc/repository/shopping_repository.dart';
+import 'package:bloc_pattern/shopping_cart_bloc/view/catalog_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'counter_bloc/counter_bloc.dart';
 import 'counter_bloc/counter_page.dart';
 import 'counter_bloc_cubit/counter_view.dart';
@@ -12,7 +14,7 @@ import 'custom_bloc_event/view_screen.dart';
 import 'home_page/home_page_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 /**
@@ -21,20 +23,30 @@ void main() {
  */
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
+  final shoppingRepository = ShoppingRepository();
   // This widget is the root of your application.
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bloc Pattern',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BlocProvider(
-        create: (_) => CustomBloc(),
-        child: const ViewScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => CatalogBloc(
+            shoppingRepository: shoppingRepository,
+          )..add(CatalogStarted()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Bloc Pattern',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (_) => CatalogPage(),
+        },
       ),
     );
   }
---
+++ lib/shopping_cart_bloc/bloc/catalog_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../models/catalog.dart';
+import '../repository/shopping_repository.dart';
+
+part 'catalog_event.dart';
+part 'catalog_state.dart';
+
+class CatalogBloc extends Bloc<CatalogEvent, CatalogState> {
+  CatalogBloc({required this.shoppingRepository}) : super(CatalogLoading()) {
+    on<CatalogStarted>(_onStarted);
+  }
+
+  final ShoppingRepository shoppingRepository;
+
+  void _onStarted(CatalogStarted event, Emitter<CatalogState> emit) async {
+    emit(CatalogLoading());
+    try {
+      final catalog = await shoppingRepository.loadCatalog();
+      emit(CatalogLoaded(Catalog(itemNames: catalog)));
+    } catch (_) {
+      emit(CatalogError());
+    }
+  }
+}
